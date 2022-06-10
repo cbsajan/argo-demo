0f51592979e3fa06b8955b1b2783fff84cd79462
C B Sajan <67416604+cbsajan@users.noreply.github.com>
2022-06-10 21:18:21 +0530

    Update name.py

--- name.py
+++ name.py
@@ -12,4 +12,4 @@ def howdy():
     <h1>{}<h1>
     """.format(name)
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host = '0.0.0.0', port=int(os.environ.get('PORT', 5000)))
