1c8a33b6652cec13ceba08c40e4da6ae26aeca3b
C B Sajan <67416604+cbsajan@users.noreply.github.com>
2022-06-10 21:24:43 +0530

    Update name.py

--- name.py
+++ name.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
 import os
 app = Flask(__name__)
+print('********')
 @app.route('/')
 def index():
     return render_template('name.html')
+
 @app.route('/howdy', methods=['POST'])
 def howdy():
     name = request.form['name']
